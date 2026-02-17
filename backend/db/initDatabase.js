5f560a35d6315b6d30e99d9d91b41d3e82f1b741
Marciah <malangwanemarciah@gmail.com>
2026-02-17 13:43:04 +0200

    connecting the server to the database

---
+++ backend/db/initDatabase.js
@@ -0,0 +1,33 @@
+const db = require('./db');
+
+db.serialize(() => {
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(150) NOT NULL UNIQUE,
+            password_hash TEXT NOT NULL,
+            role VARCHAR(20) NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS tickets (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            priority TEXT DEFAULT 'low',
+            status TEXT DEFAULT 'open',
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )
+    `);
+
+});
+
+module.exports = db;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const db = require("../db/db");
+const db = require("../db/initDatabase");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
