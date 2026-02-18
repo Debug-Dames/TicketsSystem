0ae2fbb7e58b5848a303fdb1d0bc6bee025a4605
Marciah0404 <malangwanemarciah@gmail.com>
2026-02-18 22:01:54 +0200

    updated db connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,14 +1,24 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+// const sqlite3 = require('sqlite3').verbose();
+// const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'database.sqlite');
+// const dbPath = path.resolve(__dirname, 'database.sqlite');
 
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        console.error('Database connection error:', err.message);
-    } else {
-        console.log('Connected to SQLite database.');
-    }
+// const db = new sqlite3.Database(dbPath, (err) => {
+//     if (err) {
+//         console.error('Database connection error:', err.message);
+//     } else {
+//         console.log('Connected to SQLite database.');
+//     }
+// });
+
+// module.exports = db;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false
 });
 
-module.exports = db;
+module.exports = pool;
