d9e263fa52d0ed187a9f51e990b82f58ad95d965
Marciah0404 <malangwanemarciah@gmail.com>
2026-02-18 23:00:23 +0200

    updated db connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -14,11 +14,15 @@
 // module.exports = db;
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV === 'production'
+//     ? { rejectUnauthorized: false }
+//     : false
+// });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
